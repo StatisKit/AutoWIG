1b327679ca703df161f1c18ae7b71b895f6214a1
Pierre Fernique <pfernique@gmail.com>
2015-01-20 09:10:34 +0100

    removed statiskit (empty) example

--- example/statiskit.py
+++
@@ -1,7 +0,0 @@
-from clang import cindex, enumerations
-
-cindex.Config.set_library_path('/usr/lib/llvm-3.5/lib')
-
-index = cindex.Index.create()
-
-translation_unit = index.parse(sys.argv[1], ['-x', 'c++', '-std=c++11', '-D__CODE_GENERATOR__'])
