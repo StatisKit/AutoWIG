8a9a7b96a47f08cb7ffd66e6d23761f16c65bf28
Pierre Fernique <pfernique@gmail.com>
2015-04-07 09:10:20 +0200

    add libtest, a simple C++ library used in order to illustrate AutoWIG

---
+++ test/libtest/example/classes.py
@@ -0,0 +1,12 @@
+import libtest
+
+rectangle = libtest.Rectangle(4,5)
+rectangle.get_height()
+rectangle.set_height(5)
+rectangle.get_height()
+rectangle.compute_area()
+
+triangle = libtest.Triangle(5,4)
+triangle.set_width(5)
+triangle.get_width()
+triangle.compute_area()
