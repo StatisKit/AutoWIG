df013dc0e5f2d6a68c915523ac5268f698620c2f
Pierre Fernique <pfernique@gmail.com>
2016-01-19 09:50:49 +0100

    purge source code

--- src/vplants/autowig/boost_python/func_thin.cpp
+++
@@ -1,7 +0,0 @@
-% if len(func) > 1:
-    % for index, overload in enumerate(func):
-        % if not overload.hidden:
-${str(overload.output)} (*${str(overload)}_${index})(${", ".join([str(input.type) for input in overload.inputs])}) = ${scope}${str(overload)};
-        % endif
-    % endfor
-% endif
