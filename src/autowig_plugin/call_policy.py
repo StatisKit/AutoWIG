1123de0ed01065e07e8b5e6ecbbb9241ae84e884
Pierre Fernique <pfernique@gmail.com>
2016-02-09 08:10:08 +0100

    modified plugin system

--- src/autowig_plugin/call_policy.py
+++
@@ -1,7 +0,0 @@
-class BoostPythonDefaultCallPolicyPlugin(object):
-    """
-
-    """
-    modulename = 'autowig.boost_python_default_call_policy'
-    name = 'default'
-    objectname = 'bost_python_default_call_policy'
