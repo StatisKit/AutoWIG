9fb0fd5f87b4538e4316479a5956621482b99483
Pierre Fernique <pfernique@gmail.com>
2016-08-26 19:21:10 +0200

    Update asg.py

--- src/py/autowig/asg.py
+++ src/py/autowig/asg.py
@@ -8,11 +8,16 @@ import re
 import warnings
 import hashlib
 from path import path
+from pkgtk import PluginManager
 
 from .tools import subclasses
 from .plugin_manager import visitor
 
 __all__ = ['AbstractSemanticGraph']
+
+
+visitor = PluginManager('autowig.visitor', brief="",
+        details="""""")
 
 def all_visitor(node):
     return True
