67d7ef9836b46229f7345aec0686dc867ebbf856
Pierre Fernique <pfernique@gmail.com>
2016-08-26 22:17:34 +0200

    Update _controller.py

--- src/py/autowig/_controller.py
+++ src/py/autowig/_controller.py
@@ -1,11 +1,21 @@
 from pkgtk.plugin import PluginManager
 
+from .asg import (HeaderProxy,
+                  TypedefProxy,
+                  EnumerationProxy,
+                  FunctionProxy,
+                  ConstructorProxy,
+                  ClassProxy,
+                  ClassTemplateSpecializationProxy,
+                  ClassTemplateProxy,
+                  VariableProxy,
+                  ClassTemplatePartialSpecializationProxy)
+ 
 controller = PluginManager('autowig.controller', brief="AutoWIG middle-end plugin_managers",
         details="""AutoWIG middle-end plugin_managers are responsible for Abstract Semantic Graph (ASG) modification from Python semantic queries.
 
 .. seealso:: :class:`autowig.AbstractSemanticGraph` for more details on ASGs""")
-
-
+                 
 def refactoring(asg):
     for function in asg.functions(free=True):
         if len(function.parameters) > 1:
