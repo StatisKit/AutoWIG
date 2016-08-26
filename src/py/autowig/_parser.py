27e5e322a2af5ae245fbc0bae5334325ba2817dd
Pierre Fernique <pfernique@gmail.com>
2016-08-26 19:34:56 +0200

    Update _parser.py

--- src/py/autowig/_parser.py
+++ src/py/autowig/_parser.py
@@ -6,6 +6,7 @@ from path import path
 from tempfile import NamedTemporaryFile
 import os
 import warnings
+from pkgtk.plugin import PluginManager
 
 from .asg import (NamespaceProxy,
                   FundamentalTypeProxy,
@@ -21,6 +22,11 @@ from .tools import subclasses
 from .plugin_manager import parser
 
 __all__ = ['pre_processing', 'post_processing']
+
+parser = PluginManager('autowig.parser', brief="AutoWIG front-end plugin_managers",
+        details="""AutoWIG front-end plugin_managers are responsible for Abstract Semantic Graph (ASG) completion from C/C++ parsing.
+
+.. seealso:: :class:`autowig.AbstractSemanticGraph` for more details on ASGs""")
 
 def pre_processing(asg, headers, flags, **kwargs):
     """Pre-processing step of an AutoWIG front-end
