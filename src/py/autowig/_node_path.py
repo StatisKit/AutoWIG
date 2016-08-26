1bc93ee6ced5378bb68bd4c9411f89843f9bf955
Pierre Fernique <pfernique@gmail.com>
2016-08-26 19:15:23 +0200

    Update _node_path.py

--- src/py/autowig/_node_path.py
+++ src/py/autowig/_node_path.py
@@ -7,6 +7,9 @@ __all__ = []
 
 from pkgtk.plugin import PluginManager
 
+node_path = PluginManager('autowig.node_path', brief="",
+        details="")
+        
 def hash_node_path( node, prefix='', suffix=''):
     if not isinstance(node, DeclarationProxy):
         raise TypeError('\'node\' parameter is not \'autowig.asg.CodeNodeProxy\' instance but a \'' + node.__class__.__name__ + '\' instance')
