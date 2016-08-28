266f70342938e5ee0bb4a40ecd636da4e93e4947
Pierre Fernique <pfernique@gmail.com>
2016-08-28 17:11:38 +0200

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -5,7 +5,3 @@ conda install python-clang --use-local -c statiskit
 # Build python-autowig recipe
 conda build conda/python-autowig -c statiskit -c conda-forge
 conda install python-autowig --use-local -c statiskit -c conda-forge
-conda remove python-autowig
-conda install python-scons -c statiskit
-scons py
-pip install -e .
