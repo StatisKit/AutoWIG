85ce9b17c04cac74ac14b887be806bdc0681709f
Pierre Fernique <pfernique@gmail.com>
2016-08-30 10:12:10 +0200

    Update examples

--- doc/conf.py
+++ doc/conf.py
@@ -297,6 +297,8 @@ intersphinx_mapping = {'http://docs.python.org/': None}
 numfig = True
 numfig_format = {'figure': 'Fig. %s', 'table': 'Tab. %s', 'code-block': 'Lst. %s'}
 
+nbsphinx_execute = 'always'
+
 rst_epilog = """
 
 """
