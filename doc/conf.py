73e1d3b3ea1482b3fc7134d6fb96677bf19eed4a
Pierre Fernique <pfernique@gmail.com>
2016-08-31 12:00:06 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -298,7 +298,7 @@ intersphinx_mapping = {'http://docs.python.org/': None}
 numfig = True
 numfig_format = {'figure': 'Fig. %s', 'table': 'Tab. %s', 'code-block': 'Lst. %s'}
 
-nbsphinx_execute = 'always'
+nbsphinx_execute = 'never'
 nbsphinx_timeout = 600
 
 rst_epilog = """
