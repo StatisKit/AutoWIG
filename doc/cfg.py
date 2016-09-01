c578c696c1f8de659a6fb618f16b462ec4a2ba1f
Pierre Fernique <pfernique@gmail.com>
2016-09-01 14:22:49 +0200

    Update examaples

--- doc/cfg.py
+++ doc/cfg.py
@@ -3,5 +3,3 @@ c.NbConvertApp.notebooks = [#"examples/basic.ipynb",
                             "examples/template.ipynb"]
 
 c.ExecutePreprocessor.timeout = 600
-
-# conda install python-autowig python-clanglite -c statiskit -c conda-forge && git clone https://github.com/StatisKit/AutoWIG.git && cd AutoWIG/doc && jupyter nbconvert --config cfg.py --to notebook --execute --inplace
