a577b1451066707c8a2c7f44c961297e9f20cdf2
Pierre Fernique <pfernique@gmail.com>
2016-09-01 13:35:16 +0200

    Update examples

--- doc/cfg.py
+++ doc/cfg.py
@@ -1,5 +1,5 @@
-c.NbConvertApp.notebooks = ["examples/basic.ipynb",
-                            "examples/subset.ipynb",
+c.NbConvertApp.notebooks = [#"examples/basic.ipynb",
+                            #"examples/subset.ipynb",
                             "examples/template.ipynb"]
 
 c.ExecutePreprocessor.timeout = 600
