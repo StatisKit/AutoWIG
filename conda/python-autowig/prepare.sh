4a43d5658f47724cea4a7b27077ca6eb1ae750cc
Pierre Fernique <pfernique@gmail.com>
2016-10-23 12:05:30 +0200

    Add patch for MacOsX

--- conda/python-autowig/prepare.sh
+++ conda/python-autowig/prepare.sh
@@ -1,11 +1,22 @@
 set -xe
 
-git checkout -b generate_test_patch
 cd ../../test
-sed -i'' -e '93,99d' test_basic.py
+
+git checkout -b generate_test_patch
+sed -i'' -e '91,97d' test_basic.py
 rm test_feedback.py
-git diff > ../conda/python-autowig/test.patch
+git diff > ../conda/python-autowig/windows.patch
 git add test_feedback.py test_basic.py
+git commit -m 'patch generated'
+git checkout master
+git branch -D generate_test_patch
+
+git checkout -b generate_test_patch
+rm test_feedback.py
+git diff > ../conda/python-autowig/macosx.patch
+rm test_subset.py
+git diff > ../conda/python-autowig/travis-ci.patch
+git add test_feedback.py test_subset.py
 git commit -m 'patch generated'
 git checkout master
 git branch -D generate_test_patch
