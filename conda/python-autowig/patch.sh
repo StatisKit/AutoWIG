0e6c15c837a9b2355f98df1c8bc5c91250f85ef0
Pierre Fernique <pfernique@gmail.com>
2017-01-25 10:33:00 +0100

    Prepare for new patches

--- conda/python-autowig/patch.sh
+++ conda/python-autowig/patch.sh
@@ -33,6 +33,11 @@ git branch -D generate_test_patch
 git checkout -b generate_test_patch
 rm test_feedback.py
 git diff > ../conda/python-autowig/osx.patch
+git add test_feedback.py test_subset.py
+git commit -m 'patch generated'
+git checkout master
+git branch -D generate_test_patch
+
 rm test_subset.py
 git diff > ../conda/python-autowig/travis-ci.patch
 git add test_feedback.py test_subset.py
