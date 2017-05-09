0f33d1c50231726a0033866a4bdd51f141d16dd0
Pierre Fernique <pfernique@gmail.com>
2017-05-09 14:32:04 +0200

    Correct basic examples

--- doc/examples/basic/src/py/basic/__init__.py
+++ doc/examples/basic/src/py/basic/__init__.py
@@ -14,7 +14,7 @@
 #                                                                                #
 ##################################################################################
 
-from __basic import BinomialDistribution, Overload
+from __basic import BinomialDistribution, Overload, ProbabilityError
 
 def _repr_latex_(self):
     return r"$\mathcal{B}\left(" + str(self.n) + ", " + str(round(self.get_pi(), 2)) + r"\right)$"
